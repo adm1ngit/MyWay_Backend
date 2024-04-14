6fb67e144b471f964d11da7baf1894a83a7bfe13
User <4dm1n217@gmail.com>
2024-04-14 13:07:57 -0700

    Create Model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth import get_user_model
+from django.conf import settings
+import secrets
 
 # Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = ("email")
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
+    
+
+class OtpToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="otps")
+    otp_code = models.CharField(max_length=6, default=secrets.token_hex(3))
+    tp_created_at = models.DateTimeField(auto_now_add=True)
+    otp_expires_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.user.first_name
