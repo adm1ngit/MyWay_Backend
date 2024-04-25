598b4126e88f3aedc3dfaa2fa6c2e4f54cf81e71
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-04-25 11:57:53 +0500

    finish_all5

---
+++ Book/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import *
+
+#------Jarimalar YHQ --------------#
+class JarimaBooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JarimaBook
+        fields = '__all__'
+
+#------Jarimalar YHQ END --------------#
+
+# -------- YHQ Qoidalar START -----------------#
+class YHQQoidalarBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = YHQQoidalarBook
+        fields = '__all__'
+
+# -------- YHQ Qoidalar END -----------------#
