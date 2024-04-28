9281bf00a7c23f303d192b2aa2ac8657fbff0f8d
admin_217 <4dm1n217@gmail.com>
2024-04-28 14:46:18 -0700

    finish urls

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import RegisterUserView, VerifyUserView, SendVerificationCodeView, VerifyVerificationCodeView, PasswordResetRequestView
+from .views import RegisterUserView, VerifyUserView, SendVerificationCodeView, VerifyVerificationCodeView, \
+    PasswordResetRequestView, LoginView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('send-verification-email/', SendVerificationCodeView.as_view(), name='send-verification-email'),
     path('verify-email/', VerifyVerificationCodeView.as_view(), name='verify-email'),
     path('password_reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path("login/", LoginView.as_view(), name="login"),
 ]
